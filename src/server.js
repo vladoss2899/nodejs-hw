f33444cfcd7ebfb1d81f8dfb89b38c44d0960449
vladoss2899 <136236185+vladoss2899@users.noreply.github.com>
2025-12-15 22:57:27 +0100

    new

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import pino from 'pino-http';
 import 'dotenv/config';
 
 const app = express();
-const PORT = process.env.PORT ?? 4787;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors());
